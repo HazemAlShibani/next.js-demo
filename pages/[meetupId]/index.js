17006a067e54bf55ff813ce2b6986f3688379a07
HazemAlShibani <hazem.alshibani03@gmail.com>
2023-11-01 09:44:31 -0500

    blocking update

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -32,7 +32,7 @@ export async function getStaticPaths() {
   client.close();
 
   return {
-    fallback: false,
+    fallback: "blocking",
     paths: meetupsId.map((meetup) => ({
       params: { meetupId: meetup._id.toString() },
     })),
